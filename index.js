8883ef8b10bb84904a2b22332f58f39cf3feb66b
JimmyJohnes <amsoliman03@outlook.com>
2023-05-11 20:50:15 +0300

    sixth commit: added a steps div to display how a problem is solved added functions to transpose the matrix added a function to subtract the minimum from each row in the matrix added a function to display the matrix

--- index.js
+++ index.js
@@ -1,27 +1,72 @@
-Array.prototype.Transpose = function() {
-    for(let i = 0; i < this.length; i++) {
+ function transpose(arr) {
+    for(let i = 0; i < arr.length; i++) {
         for(let j=0;j<i;j++)
         {
-            let temp = this[i][j];
-            this[i][j] = this[j][i];
-            this[j][i] = temp;
+            let temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
         }
     }
-    return this;
+    return arr;
 }
-let table = [];
+function displayMatrix()
+{
+    let string = "<table id = \"in-Steps-Table\">";
+    for(let i in matrix)
+    {
+        string += "<tr>";
+        
+        for(let j in matrix[i])
+        {
+            string += `<td>${matrix[i][j]}</td>`;
+        }
+        string += "</tr>";
+    }
+    string += "</table>";
+    return string;
+}
+function Subtract(arr)
+{
+    for(let i in arr)
+    {
+        let max = Math.min(...arr[i]);
+        for(let j in arr[i])
+        {
+            arr[i][j] = arr[i][j] - max;
+        }
+    }
+}
+let matrix = [];
 let n = localStorage.getItem("n");
 setMatrix();
 function solve()
 {
-
+    /* grab elements from The DOM */
     let stepdiv = document.querySelector("#steps");
     stepdiv.innerHTML = "";
     stepdiv.innerHTML += "<p>Starting Matrix:</p>";
     let table = document.querySelector('table');
-    for(let i =0;i<10;i++);
-    let string = "<table>"
-    stepdiv.innerHTML += string;
+    /* Set up the matrix as global variable */
+    for(let i =0 ; i<table.rows.length;i++)
+    {
+        let temp = [];
+        for(let j = 0;j<table.rows[i].cells.length;j++)
+        {
+            temp.push(document.querySelector(`#i${i}${j}`).value);
+        }
+        matrix.push(temp);
+    }
+    /* Display The first Matrix */
+    stepdiv.innerHTML += displayMatrix();
+    stepdiv.innerHTML+= "<p>Subtract The Maximum in each Row</p>"
+    Subtract(matrix);
+    stepdiv.innerHTML += displayMatrix();
+    stepdiv.innerHTML+= "<p>Subtract The Maximum in each Column</p>"
+    transpose(matrix);
+    Subtract(matrix);
+    transpose(matrix);
+    stepdiv.innerHTML += displayMatrix();
+
 }
 
 function setMatrix()
@@ -34,7 +79,7 @@ function setMatrix()
         row+="<tr>";
         for(let j=0;j<n;j++)
         {
-            row+=`<td><input type = "text" id="${i},${j}" placeholder="0" /></td>`;
+            row+=`<td><input type = "text" id="i${i}${j}" placeholder="0" /></td>`;
         }
         row+="</tr>";
     }
