4e0b303f6cf07d8f6351d0dcf0329999381b6c44
JimmyJohnes <amsoliman03@outlook.com>
2023-05-11 20:13:11 +0300

    fifth commit: set it so the matrix is set from local storage rather than a function call every time a page refreshes started woking on the solving algorithm

--- index.js
+++ index.js
@@ -9,19 +9,35 @@ Array.prototype.Transpose = function() {
     }
     return this;
 }
+let table = [];
+let n = localStorage.getItem("n");
+setMatrix();
 function solve()
 {
+
+    let stepdiv = document.querySelector("#steps");
+    stepdiv.innerHTML = "";
+    stepdiv.innerHTML += "<p>Starting Matrix:</p>";
     let table = document.querySelector('table');
+    for(let i =0;i<10;i++);
+    let string = "<table>"
+    stepdiv.innerHTML += string;
 }
 
 function setMatrix()
 {
     let matrix = document.querySelector("table");
-    let n = document.querySelector("#n").value;
-    let row = "<tr>";
-    let cell = "<td><input type=\"text\" placeholder=\"0\" /></td>".repeat(n)||localStorage.getItem("n");
-    row += cell + "</tr>";
-    row = row.repeat(n);
+    n = document.querySelector("#n").value;
+    let row = "";
+    for(let i =0;i<n;i++)
+    {
+        row+="<tr>";
+        for(let j=0;j<n;j++)
+        {
+            row+=`<td><input type = "text" id="${i},${j}" placeholder="0" /></td>`;
+        }
+        row+="</tr>";
+    }
     localStorage.setItem("n",n);
     matrix.innerHTML = row;
 }
