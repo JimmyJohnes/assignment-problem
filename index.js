6521967e65fab9c73b10cf3076852d0842058a86
JimmyJohnes <amsoliman03@outlook.com>
2023-05-11 19:38:14 +0300

    third commit: added a function to the array prototype to transpose the array changed more in css

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
+Array.prototype.Transpose = function() {
+
+}
+
+
 function solve()
 {
     let table = document.querySelector('table');
@@ -9,7 +14,7 @@ function setMatrix()
     let matrix = document.querySelector("table");
     let n = document.querySelector("#n").value;
     let row = "<tr>";
-    let cell = "<td><input type=\"text\"/></td>".repeat(n)||localStorage.getItem("n");
+    let cell = "<td><input type=\"text\" placeholder=\"0\" /></td>".repeat(n)||localStorage.getItem("n");
     row += cell + "</tr>";
     row = row.repeat(n);
     localStorage.setItem("n",n);
